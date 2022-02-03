a044c5a9df4eb936b26e282ab2d5f0d6d08fad28
Jumgalbek Arymbaev <Jumgalbek.Arymbaev@ulta.com>
2022-02-02 21:10:48 -0600

    add prisma complete

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,3 +1,17 @@
+import { Context } from '../index'
+import { Post } from '@prisma/client'
+
+
 export const Query = {
-    hello: () => 'Hello thereeeeeee'
+    hello: () => 'Hello thereeeeeee',
+    posts: async (_: any, __: any, { prisma }: Context): Promise<Post[]> => {
+
+        return prisma.post.findMany({
+            orderBy: [
+                {
+                    createdAt: "desc"
+                }
+            ]
+        })
+    }
 }
--- src/schema.ts
+++ src/schema.ts
@@ -3,6 +3,50 @@ import { gql } from 'apollo-server'
 export const typeDefs = gql`
 type Query {
     hello: String!
+    posts: [Post!]!
+}
+
+type User {
+    id: ID!     
+    email: String!   
+    name: String!   
+    posts: [Post!]!    
+    profile: Profile!
+}
+
+type Mutation {
+    postCreate(post: PostInput!): PostPayload!
+    postUpdate(postId: ID!, post: PostInput!): PostPayload!
+    postDelete(postId: ID!): PostPayload!
+}
+
+type Post {
+    id: ID!      
+    title: String!   
+    content: String!   
+    published: Boolean!
+    createdAt: String!
+    user: User!
+}
+
+type Profile {
+    id: ID!      
+    bio: String!   
+   user: User!
+}
+
+type UserError {
+    message: String!
+}
+
+type PostPayload {
+    userErrors: [UserError!]!
+    post: Post
+}
+
+input PostInput {
+    title: String
+    content: String
 }
 `
 
